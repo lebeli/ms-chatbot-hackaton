28dbeacd7660b78ae36092911157e2a0d92748c6
Benjamin Sauter <bs090@hdm-stuttgart.de>
2019-09-29 14:19:36 +0200

    cleaned up Help-Dialog.js && HelpCard

--- FestoBot/dialogs/help-dialog.js
+++ FestoBot/dialogs/help-dialog.js
@@ -6,19 +6,15 @@ const {
     ChoicePrompt,
     ListStyle,
     ComponentDialog,
-    ConfirmPrompt,
     DialogSet,
     DialogTurnStatus,
-    NumberPrompt,
     TextPrompt,
     WaterfallDialog
 } = require("botbuilder-dialogs");
 const { CardFactory } = require("botbuilder");
 
 const CHOICE_PROMPT = "CHOICE_PROMPT";
-const CONFIRM_PROMPT = "CONFIRM_PROMPT";
 const TEXT_PROMPT = "TEXT_PROMPT";
-const NUMBER_PROMPT = "NUMBER_PROMPT";
 const WATERFALL_DIALOG = "WATERFALL_DIALOG";
 const ROOT_DIALOG_ID = "HELP_ID"; // purpose?
 const HelpCard = require("../resources/HelpCard.json");
@@ -31,8 +27,6 @@ class HelpDialog extends ComponentDialog {
 
         this.addDialog(new TextPrompt(TEXT_PROMPT));
         this.addDialog(new ChoicePrompt(CHOICE_PROMPT));
-        this.addDialog(new ConfirmPrompt(CONFIRM_PROMPT));
-        this.addDialog(new NumberPrompt(NUMBER_PROMPT, this.agePromptValidator));
 
         this.addDialog(new WaterfallDialog(WATERFALL_DIALOG, [
             this.helpCardStep.bind(this),
@@ -83,83 +77,6 @@ class HelpDialog extends ComponentDialog {
         } else {
             return step.endDialog();
         }
-    }
-
-    async transportStep (step) {
-        // WaterfallStep always finishes with the end of the Waterfall or with another dialog; here it is a Prompt Dialog.
-        // Running a prompt here means the next WaterfallStep will be run when the users response is received.
-        return step.prompt(CHOICE_PROMPT, {
-            prompt: "Please enter your mode of transport.",
-            choices: ChoiceFactory.toChoices(["Car", "Bus", "Bicycle"])
-        });
-    }
-
-    async nameStep (step) {
-        step.values.transport = step.result.value;
-        return step.prompt(TEXT_PROMPT, "What is your name, human?");
-    }
-
-    async nameConfirmStep (step) {
-        step.values.name = step.result;
-
-        // We can send messages to the user at any point in the WaterfallStep.
-        await step.context.sendActivity(`Thanks ${step.result}.`);
-
-        // WaterfallStep always finishes with the end of the Waterfall or with another dialog; here it is a Prompt Dialog.
-        return step.prompt(CONFIRM_PROMPT, "Do you want to give your age?", ["yes", "no"]);
-    }
-
-    async ageStep (step) {
-        if (step.result) {
-            // User said "yes" so we will be prompting for the age.
-            // WaterfallStep always finishes with the end of the Waterfall or with another dialog, here it is a Prompt Dialog.
-            const promptOptions = { prompt: "Please enter your age.", retryPrompt: "The value entered must be greater than 0 and less than 150." };
-
-            return step.prompt(NUMBER_PROMPT, promptOptions);
-        } else {
-            // User said "no" so we will skip the next step. Give -1 as the age.
-            return step.next(-1);
-        }
-    }
-
-    async confirmStep (step) {
-        step.values.age = step.result;
-
-        const msg = step.values.age === -1 ? "No age given." : `I have your age as ${step.values.age}.`;
-
-        // We can send messages to the user at any point in the WaterfallStep.
-        await step.context.sendActivity(msg);
-
-        // WaterfallStep always finishes with the end of the Waterfall or with another dialog, here it is a Prompt Dialog.
-        return step.prompt(CONFIRM_PROMPT, { prompt: "Is this okay?" });
-    }
-
-    async summaryStep (step) {
-        if (step.result) {
-            // Get the current profile object from user state.
-            // const userProfile = await this.userProfile.get(step.context, new UserProfile());
-
-            const transport = step.values.transport;
-            const name = step.values.name;
-            const age = step.values.age;
-
-            let msg = `I have your mode of transport as ${transport} and your name as ${name}.`;
-            if (age !== -1) {
-                msg += ` And age as ${age}.`;
-            }
-
-            await step.context.sendActivity(msg);
-        } else {
-            await step.context.sendActivity("Thanks. Your profile will not be kept.");
-        }
-
-        // WaterfallStep always finishes with the end of the Waterfall or with another dialog, here it is the end.
-        return step.endDialog();
-    }
-
-    async agePromptValidator (promptContext) {
-        // This condition is our validation rule. You can also change the value at this point.
-        return promptContext.recognized.succeeded && promptContext.recognized.value > 0 && promptContext.recognized.value < 150;
     }
 }
 
